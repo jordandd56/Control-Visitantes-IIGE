9b2b7fbd604dd7f6310ac8f6a4ff19eb3e29a617
DILAN JORDAN <djsegovia@puce.edu.ec>
2025-06-19 21:58:17 -0500

    Agregar restricción CHECK en inserciones.sql para validar que cédula o pasaporte tenga valor

--- backend/database/02_inserciones.sql
+++ backend/database/02_inserciones.sql
@@ -8,3 +8,12 @@ INSERT INTO areas (id, nombre) VALUES
     (1, 'Administración'),
     (2, 'Seguridad'),
     (3, 'Recepción');
+
+-- Aplicacion de restricciones para que cedula o pasaporte no sean nulos
+-- Esto asegura que al menos uno de los dos campos debe tener un valor
+    ALTER TABLE visitantes
+ADD CONSTRAINT chk_cedula_o_pasaporte
+CHECK (
+  cedula IS NOT NULL OR pasaporte IS NOT NULL
+);
+
