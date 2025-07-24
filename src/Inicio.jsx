0630981beea045eb7270f224a1100766808506b1
joseframuzo <jose.muzo@live.com>
2025-07-23 20:56:00 -0500

    Actualización automática en pantalla al agregar un usuario nuevo

--- src/Inicio.jsx
+++ src/Inicio.jsx
@@ -10,14 +10,13 @@ const API_BASE_VISITANTES =
 const API_BASE_AREAS =
   "https://mi-backend-nodejs-c0d5dre0cwgughb4.centralus-01.azurewebsites.net/api/areas";
 
-//elimniar boton de reporte
-
 
 const normalizarVisita = (v) => {
   let estadoUI = "Pendiente";
   if (v.estado === "ingreso") {
     estadoUI = v.hora_salida ? "Salió" : "Aprobado";
-  
+  } else if (v.estado === "salida") {
+    estadoUI = "Salió";
   } else if (v.estado === "rechazado") {
     estadoUI = "Rechazado";
   }
@@ -62,8 +61,6 @@ function exportCSV(filas) {
     "Salida",
     "Observaciones",
   ];
-
-  // boton de busqueda de usuario
   const csv = [
     headers,
     ...filas.map((r) => [
